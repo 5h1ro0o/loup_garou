d695534d3cdc58e81324a5eb54191a826fb39332
Matteo Robin <120198629+5h1ro0o@users.noreply.github.com>
2025-02-05 10:36:58 +0100

    test

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+class LoupGarou : 
+
+    def __init__(self, name, role, is_alive):
+        self.name = name
+        self.role = role
+        self.is_alive = is_alive
+    
+    def hello(self):
+        print('Hello, I am a Loup Garou')
+
 if __name__ == '__main__':
-    print('Loup Garou')
+    loup = LoupGarou('Loup', 'LoupGarou', True)
+    loup.hello()
